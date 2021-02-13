9ae718e25b5953c1ebfa75829d391150406ca053
umutlion <umutcayli@outlook.com>
2021-02-14 00:46:41 +0300

    created Post Model, API views and serializers.

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pass
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    pass
